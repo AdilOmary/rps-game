76b5a7e96d81829636d5d9a9c41678bd772695a6
Adil Omary <adilomary@hotmail.fr>
2021-06-29 12:46:19 +0200

    Model migration and validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,17 @@
 class Game < ApplicationRecord
+  belongs_to :user
+
+  enum move: {
+    rock: :rock,
+    paper: :paper,
+    scissor: :scissor
+  }
+
+  enum bot_move: {
+    rock: :rock,
+    paper: :paper,
+    scissor: :scissor
+  }, _prefix: true
+
+  validates :move, presence: true
 end
--- db/migrate/20210629094930_create_games.rb
+++ db/migrate/20210629094930_create_games.rb
@@ -1,6 +1,7 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
+      t.belongs_to :user
       t.string :move
       t.string :bot_move
 
