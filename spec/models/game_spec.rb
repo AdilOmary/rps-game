9abcd00920a63a92edb37b513710524757a4f26e
Adil Omary <adilomary@hotmail.fr>
2021-06-30 12:26:17 +0200

    Adding new tests and correcting the first one

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  it 'returns false if rock is the object and rock is the argument' do
+  it 'returns draw if rock if both players plays rock' do
     game = Game.new(move: 'rock', bot_move: 'rock', user: User.new(name: 'Player'))
-    expect(game.who_wins).to eq("It's a dra")
+    expect(game.who_wins).to eq("It's a draw")
+  end
+
+  it 'returns player win he plays rock and the bot plays scissors' do
+    game = Game.new(move: 'rock', bot_move: 'scissors', user: User.new(name: 'Player'))
+    expect(game.who_wins).to eq("Player wins")
   end
 end
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Games", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Games', type: :request do
+  describe 'GET /index' do
+    it 'returns the moves' do
+      post '/games', params: { move: 'rock', name: 'Player' }
+      expect(response).to have_http_status(:created)
+    end
   end
 end
