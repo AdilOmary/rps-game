bf9211164a6a65cbd344e70e80b58bcf392d9675
Adil Omary <adilomary@hotmail.fr>
2021-06-30 14:34:59 +0200

    Adding test and correcting typos

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  it 'returns draw if rock if both players plays rock' do
+  it 'returns draw if both players plays rock' do
     game = Game.new(move: 'rock', bot_move: 'rock', user: User.new(name: 'Player'))
     expect(game.who_wins).to eq("It's a draw")
   end
 
-  it 'returns player win he plays rock and the bot plays scissors' do
+  it 'returns player win when he plays rock and the bot plays scissors' do
     game = Game.new(move: 'rock', bot_move: 'scissors', user: User.new(name: 'Player'))
     expect(game.who_wins).to eq("Player wins")
+  end
+
+  it 'returns player lose when he plays scissors and the bot plays rock' do
+    game = Game.new(move: 'scissors', bot_move: 'rock', user: User.new(name: 'Player'))
+    expect(game.who_wins).to eq("Bot wins !")
   end
 end
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe 'Games', type: :request do
       post '/games', params: { move: 'rock', name: 'Player' }
       expect(response).to have_http_status(:created)
     end
+
+    it "create a game with player's move and the user" do
+      expect {
+        post '/games', params: { move: 'rock', name: 'adil'}
+      }.to change(Game, :count).by(1)
+    end
   end
 end
